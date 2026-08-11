5d025840c5573f4f13c1f71b00f031f901998c07
pakashiva <pakashivaprasad2004@gmail.com>
2026-08-11 15:20:23 +0530

    changes in database config

--- app_configuration/app_config.py
+++ app_configuration/app_config.py
@@ -19,3 +19,8 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_pre_ping": True,
+    "pool_recycle": 1800,
+}
